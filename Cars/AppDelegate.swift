040bd44cbd56bc11087ba50dfe8851baef65fd1f
RaviSeta-at-237320554477 <ravi.seta@yobored.com>
2022-07-15 12:24:50 +0530

    Jail broken condition added in app delegate

--- Cars/AppDelegate.swift
+++ Cars/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        if !UIDevice.current.isJailBroken {
+        if UIDevice.current.isJailBroken {
             exit(0)
         }
         
